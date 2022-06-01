6afda3aba12ea2b1f3a270ee5beda2d81294b50f
qwe5507 <apvmffkdls@gmail.com>
2022-06-01 19:47:51 +0900

    서블릿과 파일 업로드2

--- src/main/java/hello/upload/controller/ServletUploadControllerV2.java
+++ src/main/java/hello/upload/controller/ServletUploadControllerV2.java
@@ -3,6 +3,8 @@ package hello.upload.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StreamUtils;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +13,8 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.Part;
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.Collection;
 
 @Slf4j
@@ -38,16 +42,26 @@ public class ServletUploadControllerV2 {
         for (Part part : parts) {
             log.info("==== PART ====");
             log.info("name={}", part.getName());
-            Collection<String> headerNames = part.getHeaderNames(
-
-            );
+            Collection<String> headerNames = part.getHeaderNames();
             for (String headerName : headerNames) {
                 log.info("header {}: {}", headerName, part.getHeader(headerName));
             }
             //편의 메서드
             //content-disposition; filename
+            log.info("submittedFileName={}", part.getSubmittedFileName());
+            log.info("size={}", part.getSize()); //part body size
 
+            //데이터 읽기
+            InputStream inputStream = part.getInputStream();
+            String body = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+            log.info("body={}", body);
 
+            //파일에 저장하기
+            if (StringUtils.hasText(part.getSubmittedFileName())) {
+                String fullPath = fileDir + part.getSubmittedFileName();
+                log.info("파일 저장 fullPath={}", fullPath);
+                part.write(fullPath);
+            }
         }
 
         return "upload-form";
